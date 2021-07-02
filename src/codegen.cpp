00da37e38aa0448bcd19f5dea4605db68460019a
Zhang Hao <zhanghao.m@bytedance.com>
2021-07-02 18:48:27 +0800

    update namespace llvm

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -1,34 +1,36 @@
 
 #include "ast.h"
 #include "log/log.h"
+
+using namespace llvm;
 namespace CodeGen{
     
-    std::unique_ptr<llvm::LLVMContext> TheContext;
-    std::unique_ptr<llvm::Module> TheModule;
-    std::unique_ptr<llvm::IRBuilder<>> Builder;
-    std::map<std::string, llvm::Value *> NamedValues;
+    std::unique_ptr<LLVMContext> TheContext;
+    std::unique_ptr<Module> TheModule;
+    std::unique_ptr<IRBuilder<>> Builder;
+    std::map<std::string, Value *> NamedValues;
 };
 
-llvm::Value *LogErrorV(const char *Str) {
+Value *LogErrorV(const char *Str) {
     LOG::ERROR(Str);
     return nullptr;
 }
 
-llvm::Value* openjs::NumberExpAst::codegen() {
-    return llvm::ConstantFP::get(*CodeGen::TheContext, llvm::APFloat(val));
+Value* openjs::NumberExpAst::codegen() {
+    return ConstantFP::get(*CodeGen::TheContext, APFloat(val));
 }
 
-llvm::Value* openjs::VariableExpAst::codegen() {
+Value* openjs::VariableExpAst::codegen() {
     // Look this variable up in the function.
-    llvm::Value *V = CodeGen::NamedValues[Name];
+    Value *V = CodeGen::NamedValues[Name];
     if (!V)
         return LogErrorV("Unknown variable name");
     return V;
 }
 
-llvm::Value* openjs::BinaryExpAst::codegen() {
-    llvm::Value *L = LHS->codegen();
-    llvm::Value *R = RHS->codegen();
+Value* openjs::BinaryExpAst::codegen() {
+    Value *L = LHS->codegen();
+    Value *R = RHS->codegen();
     if (!L || !R)
         return nullptr;
 
@@ -50,40 +52,84 @@ llvm::Value* openjs::BinaryExpAst::codegen() {
     }
 }
 
-llvm::Value* openjs::AssignExpAst::codegen(){
-    // var = std::move(RHS->codegen());
-    return nullptr;
+Value* openjs::AssignExpAst::codegen(){
+
+    std::vector<AllocaInst *> OldBindings;
+
+    Function *TheFunction = Builder->GetInsertBlock()->getParent();
+
+    // Register all variables and emit their initializer.
+    for (unsigned i = 0, e = VarNames.size(); i != e; ++i) {
+        const std::string &VarName = VarNames[i].first;
+        ExprAST *Init = VarNames[i].second.get();
+
+        // Emit the initializer before adding the variable to scope, this prevents
+        // the initializer from referencing the variable itself, and permits stuff
+        // like this:
+        //  var a = 1 in
+        //    var a = a in ...   # refers to outer 'a'.
+        Value *InitVal;
+        if (Init) {
+        InitVal = Init->codegen();
+        if (!InitVal)
+            return nullptr;
+        } else { // If not specified, use 0.0.
+        InitVal = ConstantFP::get(*TheContext, APFloat(0.0));
+        }
+
+        AllocaInst *Alloca = CreateEntryBlockAlloca(TheFunction, VarName);
+        Builder->CreateStore(InitVal, Alloca);
+
+        // Remember the old variable binding so that we can restore the binding when
+        // we unrecurse.
+        OldBindings.push_back(NamedValues[VarName]);
+
+        // Remember this binding.
+        NamedValues[VarName] = Alloca;
+    }
+
+    // Codegen the body, now that all vars are in scope.
+    Value *BodyVal = Body->codegen();
+    if (!BodyVal)
+        return nullptr;
+
+    // Pop all our variables from scope.
+    for (unsigned i = 0, e = VarNames.size(); i != e; ++i)
+        NamedValues[VarNames[i].first] = OldBindings[i];
+
+    // Return the body computation.
+    return BodyVal;
 }
 
-llvm::Value* openjs::MutiExpAst::codegen(){
+Value* openjs::MutiExpAst::codegen(){
     for(int i = 0; i < (int)exp.size(); i++)
         exp[i]->codegen();
     return nullptr;
 }
 
-llvm::Value* openjs::IfExpAst::codegen(){
-    llvm::Value *CondV = condition->codegen();
+Value* openjs::IfExpAst::codegen(){
+    Value *CondV = condition->codegen();
     if (!CondV)
         return nullptr;
 
     // Convert condition to a bool by comparing non-equal to 0.0.
     CondV = CodeGen::Builder->CreateFCmpONE(
-        CondV, llvm::ConstantFP::get(*CodeGen::TheContext, llvm::APFloat(0.0)), "ifcond");
+        CondV, ConstantFP::get(*CodeGen::TheContext, APFloat(0.0)), "ifcond");
 
-    llvm::Function *TheFunction = CodeGen::Builder->GetInsertBlock()->getParent();
+    Function *TheFunction = CodeGen::Builder->GetInsertBlock()->getParent();
 
     // Create blocks for the then and else cases.  Insert the 'then' block at the
     // end of the function.
-    llvm::BasicBlock *ThenBB = llvm::BasicBlock::Create(*CodeGen::TheContext, "then", TheFunction);
-    llvm::BasicBlock *ElseBB = llvm::BasicBlock::Create(*CodeGen::TheContext, "else");
-    llvm::BasicBlock *MergeBB = llvm::BasicBlock::Create(*CodeGen::TheContext, "ifcont");
+    BasicBlock *ThenBB = BasicBlock::Create(*CodeGen::TheContext, "then", TheFunction);
+    BasicBlock *ElseBB = BasicBlock::Create(*CodeGen::TheContext, "else");
+    BasicBlock *MergeBB = BasicBlock::Create(*CodeGen::TheContext, "ifcont");
 
     CodeGen::Builder->CreateCondBr(CondV, ThenBB, ElseBB);
 
     // Emit then value.
     CodeGen::Builder->SetInsertPoint(ThenBB);
 
-    llvm::Value *ThenV = if_exp->codegen();
+    Value *ThenV = if_exp->codegen();
     if (!ThenV)
         return nullptr;
 
@@ -95,7 +141,7 @@ llvm::Value* openjs::IfExpAst::codegen(){
     TheFunction->getBasicBlockList().push_back(ElseBB);
     CodeGen::Builder->SetInsertPoint(ElseBB);
 
-    llvm::Value *ElseV = else_exp->codegen();
+    Value *ElseV = else_exp->codegen();
     if (!ElseV)
         return nullptr;
 
@@ -106,7 +152,7 @@ llvm::Value* openjs::IfExpAst::codegen(){
     // Emit merge block.
     TheFunction->getBasicBlockList().push_back(MergeBB);
     CodeGen::Builder->SetInsertPoint(MergeBB);
-    llvm::PHINode *PN = CodeGen::Builder->CreatePHI(llvm::Type::getDoubleTy(*CodeGen::TheContext), 2, "iftmp");
+    PHINode *PN = CodeGen::Builder->CreatePHI(Type::getDoubleTy(*CodeGen::TheContext), 2, "iftmp");
 
     PN->addIncoming(ThenV, ThenBB);
     PN->addIncoming(ElseV, ElseBB);
@@ -114,9 +160,9 @@ llvm::Value* openjs::IfExpAst::codegen(){
 
 }
 
-llvm::Value* openjs::CallExpAst::codegen() {
+Value* openjs::CallExpAst::codegen() {
     // Look up the name in the global module table.
-    llvm::Function *CalleeF = CodeGen::TheModule->getFunction(Callee);
+    Function *CalleeF = CodeGen::TheModule->getFunction(Callee);
     if (!CalleeF)
         return LogErrorV("Unknown function referenced");
 
@@ -124,7 +170,7 @@ llvm::Value* openjs::CallExpAst::codegen() {
     if (CalleeF->arg_size() != Args.size())
         return LogErrorV("Incorrect # arguments passed");
 
-    std::vector<llvm::Value *> ArgsV;
+    std::vector<Value *> ArgsV;
     for (unsigned i = 0, e = Args.size(); i != e; ++i) {
         ArgsV.push_back(Args[i]->codegen());
         if (!ArgsV.back())
@@ -135,16 +181,16 @@ llvm::Value* openjs::CallExpAst::codegen() {
     return nullptr;
 }
 
-llvm::Function* openjs::PrototypeAst::codegen() {
+Function* openjs::PrototypeAst::codegen() {
     // Make the function type:  double(double,double) etc.
-    std::vector<llvm::Type *> Doubles(Args.size(), llvm::Type::getDoubleTy(*CodeGen::TheContext));
-    llvm::FunctionType *FT =
-        llvm::FunctionType::get(llvm::Type::getDoubleTy(*CodeGen::TheContext), Doubles, false);
+    std::vector<Type *> Doubles(Args.size(), Type::getDoubleTy(*CodeGen::TheContext));
+    FunctionType *FT =
+        FunctionType::get(Type::getDoubleTy(*CodeGen::TheContext), Doubles, false);
 
-    //llvm::FunctionType * function_type = llvm::FunctionType::get();
+    //FunctionType * function_type = FunctionType::get();
 
-    llvm::Function *F =
-        llvm::Function::Create(FT, llvm::Function::ExternalLinkage, Name, CodeGen::TheModule.get());
+    Function *F =
+        Function::Create(FT, Function::ExternalLinkage, Name, CodeGen::TheModule.get());
 
     // Set names for all arguments.
     unsigned Idx = 0;
@@ -154,9 +200,9 @@ llvm::Function* openjs::PrototypeAst::codegen() {
     return F;
 }
 
-llvm::Function* openjs::FunctionAst::codegen() {
+Function* openjs::FunctionAst::codegen() {
     // First, check for an existing function from a previous 'extern' declaration.
-    llvm::Function *TheFunction = CodeGen::TheModule->getFunction(Proto->getName());
+    Function *TheFunction = CodeGen::TheModule->getFunction(Proto->getName());
 
     if (!TheFunction)
         TheFunction = Proto->codegen();
@@ -165,7 +211,7 @@ llvm::Function* openjs::FunctionAst::codegen() {
         return nullptr;
 
     // Create a new basic block to start insertion into.
-    llvm::BasicBlock *BB = llvm::BasicBlock::Create(*CodeGen::TheContext, "entry", TheFunction);
+    BasicBlock *BB = BasicBlock::Create(*CodeGen::TheContext, "entry", TheFunction);
     CodeGen::Builder->SetInsertPoint(BB);
 
     // Record the function arguments in the NamedValues map.
@@ -173,7 +219,7 @@ llvm::Function* openjs::FunctionAst::codegen() {
     for (auto &Arg : TheFunction->args())
         CodeGen::NamedValues[std::string(Arg.getName())] = &Arg;
 
-    if (llvm::Value *RetVal = Body->codegen()) {
+    if (Value *RetVal = Body->codegen()) {
         // Finish off the function.
         // Builder->CreateRet(RetVal);
 
